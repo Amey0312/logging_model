86b709bc49f67ee85e3bceae3fba6f29a17f8c22
amey <ameynitore5543@gmail.com>
2025-01-27 22:37:32 +0530

    Updated the file structure

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,10 @@ app.use('/api/items', itemRoutes);
 const PORT = process.env.PORT || 5000;
 
 // Serve static files from the Next.js build
-app.use(express.static(path.join(__dirname, "../frontend/out")));
+app.use(express.static(path.join(__dirname, "../my-app/out")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/out/index.html"));
+  res.sendFile(path.join(__dirname, "../my-app/out/index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
